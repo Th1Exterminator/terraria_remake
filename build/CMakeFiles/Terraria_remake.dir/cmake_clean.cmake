e512d4f346cec1a170613cf19e492db004a49396
Th1Exterminator <atillmand@gmail.com>
2024-04-19 02:56:09 -0500

    Hello World

---
+++ build/CMakeFiles/Terraria_remake.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Terraria_remake.dir/src/main.cpp.o"
+  "CMakeFiles/Terraria_remake.dir/src/main.cpp.o.d"
+  "Terraria_remake"
+  "Terraria_remake.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Terraria_remake.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_video.h>
+#include <stdio.h>
+
+const int _WIDTH  = 800;
+const int _HEIGHT = 600;
+
+int main(int argc, const char** argv)
+{
+    // Create SDL window
+    SDL_Window* window = NULL;
+
+    return 0;
+}
